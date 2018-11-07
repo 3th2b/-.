b6740d22353358efe82f21f6084822a611fb45ea
3th2b <44846119+3th2b@users.noreply.github.com>
2018-11-07 18:33:43 +0100

    Add files via upload

---
+++ bottt.js
@@ -0,0 +1,43 @@
+﻿
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const adminprefix = "-";
+const developers = ['498063352426135572'];
+
+console.log("3thhh2bbb.");
+
+
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`Changing The Playing To : **${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`Changing The Watching To : **${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`Changing The Listening To : **${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/inikolaiii");
+      message.channel.send(`Changing The Streaming To : **${argresult}**`)
+  }
+  if (message.content.startsWith(adminprefix + 'sn')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}**`)
+} else
+if (message.content.startsWith(adminprefix + 'sa')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To : **${argresult}**`);
+}
+});
+
+client.login("NDk4MDYzMzUyNDI2MTM1NTcy.DsSOpQ.OhBI3-LiYe8azs3AxHllTAaMk0M")  
